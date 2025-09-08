40eb1fd095b1d58f2ce2e1c1ca911978ddd510e4
MontebellerZ <vyatrhjar@gmail.com>
2025-09-08 11:22:06 -0300

    feat: figurinhas repetidas provisório

--- app/(tabs)/repetidas/_layout.tsx
+++ app/(tabs)/repetidas/_layout.tsx
@@ -1,9 +1,22 @@
+import Mozinho from "@/assets/imgs/mozinho.jpg";
+import { Image } from "expo-image";
 import { Text, View } from "react-native";
+import styles from "./styles";
 
 export default function Repetidas() {
   return (
-    <View>
-      <Text>Repetidas</Text>
+    <View style={styles.container}>
+      <View style={styles.titleHolder}>
+        <Text style={styles.title}>Figurinhas repetidas</Text>
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.text}>Está tela ainda está em construção</Text>
+
+        <Text style={styles.text}>❤ Aguarde minha amorzinha 💕</Text>
+
+        <Image source={Mozinho} style={styles.imagem} />
+      </View>
     </View>
   );
 }
--- app/(tabs)/repetidas/styles.ts
+++ app/(tabs)/repetidas/styles.ts
@@ -1,3 +1,40 @@
+import Colors from "@/utils/colors";
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.transparency(Colors.background, 0.85),
+    borderRadius: 16,
+    padding: 16,
+    gap: 10,
+  },
+  titleHolder: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    color: Colors.texto,
+  },
+  content: {
+    flex: 1,
+    gap: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+  },
+  imagem: {
+    width: "90%",
+    aspectRatio: 1,
+    elevation: 4,
+    borderRadius: 100,
+    overflow: "hidden",
+  },
+});
